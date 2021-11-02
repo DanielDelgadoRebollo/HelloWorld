7e9bcda1b582de7fa7de3907c3a82879cdde202f
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2021-11-02 09:37:12 +0100

    subida

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Creamos una variable
+
+    Button b1 ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asociar el boton
+        b1 = findViewById(R.id.button2);
+
+        //  Generar una acción al pinchar el botón
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button1;
+    private EditText txt1;
+    public static final String MENSAJE = "texto del saludo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = findViewById(R.id.button);
+        txt1 = findViewById(R.id.texto1);
     }
+
+    public void saludar(View view) {
+        Intent intent = new Intent(this,SegundaActividad.class);
+        String mensaje = String.valueOf(txt1.getText());
+        intent.putExtra(MENSAJE,mensaje);
+        startActivity(intent);
+    }
+
 }
