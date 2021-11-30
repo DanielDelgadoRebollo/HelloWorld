66cbf13150515f2f54025d74b14a12cf2b917036
DanielDelgadoRebollo <daniel.delgado@cesjuanpablosegundo.es>
2021-12-01 00:00:19 +0100

    subida

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -12,9 +12,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     TextView textView;
-    Button btnCero,btnUno,btnDos,btnTres,btnCuatro,btnCinco,btnSeis,btnSiete,btnOcho,btnNueve,btnSuma,btnResta,btnDivision,btnMultiplicacion,btnIgual,btnBorrar,btnPorcentaje,btnMasMenos,btnComa;
-    int valorUno=0,valorDos=0;
-    char c='a';
+    Button btnCero, btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve, btnSuma, btnResta, btnDivision, btnMultiplicacion, btnIgual, btnBorrar, btnPorcentaje, btnMasMenos, btnComa
+            ,btnSin,btnCos,btnTan,btnLn;
+    int valorUno = 0, valorDos = 0;
+    char c = 'a';
 
 
     @Override
@@ -25,7 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         accion();
     }
 
-    public void instanciar(){
+    public void instanciar() {
         textView = findViewById(R.id.textView);
         btnCero = findViewById(R.id.btnCero);
         btnUno = findViewById(R.id.btnUno);
@@ -38,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnOcho = findViewById(R.id.btnOcho);
         btnNueve = findViewById(R.id.btnNueve);
         btnSuma = findViewById(R.id.btnSuma);
-        btnResta= findViewById(R.id.btnResta);
+        btnResta = findViewById(R.id.btnResta);
         btnMultiplicacion = findViewById(R.id.btnMultiplicar);
         btnDivision = findViewById(R.id.btnDividir);
         btnComa = findViewById(R.id.btnComa);
@@ -46,44 +47,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnBorrar = findViewById(R.id.btnBorrar);
         btnMasMenos = findViewById(R.id.btnMasMenos);
         btnIgual = findViewById(R.id.btnIgual);
+        btnSin = findViewById(R.id.btnSin);
+        btnCos = findViewById(R.id.btnCos);
+        btnTan = findViewById(R.id.btnTan);
+        btnLn = findViewById(R.id.btnTan);
+
     }
 
-    public void accion(){
+    public void accion() {
+        btnCero.setOnClickListener(this);
+        btnUno.setOnClickListener(this);
         btnDos.setOnClickListener(this);
+        btnTres.setOnClickListener(this);
+        btnCuatro.setOnClickListener(this);
+        btnCinco.setOnClickListener(this);
+        btnSeis.setOnClickListener(this);
+        btnSiete.setOnClickListener(this);
+        btnOcho.setOnClickListener(this);
+        btnNueve.setOnClickListener(this);
+        btnSuma.setOnClickListener(this);
+        btnResta.setOnClickListener(this);
+        btnMultiplicacion.setOnClickListener(this);
+        btnDivision.setOnClickListener(this);
+        btnBorrar.setOnClickListener(this);
+        btnIgual.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnUno:
-                textView.setText(textView.getText()+"1");
+                textView.setText(textView.getText() + "1");
                 break;
             case R.id.btnDos:
-                textView.setText(textView.getText()+"2");
+                textView.setText(textView.getText() + "2");
                 break;
             case R.id.btnTres:
-                textView.setText(textView.getText()+"3");
+                textView.setText(textView.getText() + "3");
                 break;
             case R.id.btnCuatro:
-                textView.setText(textView.getText()+"4");
+                textView.setText(textView.getText() + "4");
                 break;
             case R.id.btnCinco:
-                textView.setText(textView.getText()+"5");
+                textView.setText(textView.getText() + "5");
                 break;
             case R.id.btnSeis:
-                textView.setText(textView.getText()+"6");
+                textView.setText(textView.getText() + "6");
                 break;
             case R.id.btnSiete:
-                textView.setText(textView.getText()+"7");
+                textView.setText(textView.getText() + "7");
                 break;
             case R.id.btnOcho:
-                textView.setText(textView.getText()+"8");
+                textView.setText(textView.getText() + "8");
                 break;
             case R.id.btnNueve:
-                textView.setText(textView.getText()+"9");
+                textView.setText(textView.getText() + "9");
                 break;
             case R.id.btnCero:
-                textView.setText(textView.getText()+"9");
+                textView.setText(textView.getText() + "0");
                 break;
             case R.id.btnSuma:
                 valorUno = Integer.parseInt(String.valueOf(textView.getText()));
@@ -109,19 +130,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 textView.setText("");
                 break;
             case R.id.btnIgual:
-                if (valorUno == 0 || textView.getText().equals("")) {
-                    valorDos = Integer.parseInt(String.valueOf(textView.getText()));
+
                     if (c == '+') {
-                        textView.setText(valorUno + valorDos);
+                        valorDos = Integer.parseInt(String.valueOf(textView.getText()));
+                        textView.setText(String.valueOf(valorUno + valorDos));
                     } else if (c == '-') {
-                        textView.setText(valorUno - valorDos);
+                        valorDos = Integer.parseInt(String.valueOf(textView.getText()));
+                        textView.setText(String.valueOf(valorUno - valorDos));
                     } else if (c == '*') {
-                        textView.setText(valorUno * valorDos);
+                        valorDos = Integer.parseInt(String.valueOf(textView.getText()));
+                        textView.setText(String.valueOf(valorUno * valorDos));
                     } else if (c == '/') {
-                        textView.setText(valorUno / valorDos);
+                        valorDos = Integer.parseInt(String.valueOf(textView.getText()));
+                        textView.setText(String.valueOf(valorUno / valorDos));
                     }
                     break;
+            case R.id.btnSin:
+                if (textView.getText()!=null){
+                    Math.sin(Double.parseDouble(String.valueOf(textView.getText())));
                 }
+                break;
+            case R.id.btnCos:
+                if (textView.getText()!=null){
+                    Math.cos(Double.parseDouble(String.valueOf(textView.getText())));
+                }
+                break;
+            case R.id.btnLn:
+                if (textView.getText()!=null){
+                    Math.log(Double.parseDouble(String.valueOf(textView.getText())));
+                }
+                break;
+            case R.id.btnTan:
+                if (textView.getText()!=null){
+                    Math.tan(Double.parseDouble(String.valueOf(textView.getText())));
+                }
+                break;
         }
     }
 }
