0f0ef3dd8101f6545b1821b10985cafb1cfce501
DanielDelgadoRebollo <72132961+DanielDelgadoRebollo@users.noreply.github.com>
2022-02-01 14:20:54 +0100

    subida

---
+++ ElementosMaterial/app/src/main/java/com/example/elementosmaterial/Fragments/FragmentUno.java
@@ -0,0 +1,22 @@
+package com.example.elementosmaterial.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.elementosmaterial.R;
+
+public class FragmentUno extends Fragment {
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_uno,container,false);
+        return view;
+    }
+}
--- ElementosMaterial/app/src/main/java/com/example/elementosmaterial/MainActivity.java
+++ ElementosMaterial/app/src/main/java/com/example/elementosmaterial/MainActivity.java
@@ -1,26 +1,37 @@
 package com.example.elementosmaterial;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.elementosmaterial.Fragments.FragmentDos;
+import com.example.elementosmaterial.Fragments.FragmentUno;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     private Toolbar toolbar;
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
+    private ImageView imageView;
+    private Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         instancias();
-        configurarToolbar();
+        acciones();
     }
 
     @Override
@@ -48,6 +59,58 @@ public class MainActivity extends AppCompatActivity {
 
     public void instancias(){
       toolbar = findViewById(R.id.toolbar);
+      configurarToolbar();
+      navigationView = findViewById(R.id.navigation_view);
+      drawerLayout = findViewById(R.id.drawer_layout);
+      imageView = navigationView.getHeaderView(0).findViewById(R.id.imageviewLetra);
+      spinner = navigationView.getHeaderView(0).findViewById(R.id.spinnerNombre);
+      ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,0,0);
+      drawerLayout.addDrawerListener(actionBarDrawerToggle);
+      actionBarDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if (drawerLayout.isOpen()){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else{
+            if (getSupportFragmentManager().getBackStackEntryCount()==0){
+                finish();
+            }else{
+                super.onBackPressed();
+            }
+        }
+    }
+
+    public void acciones(){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+                switch (item.getItemId()){
+                    case R.id.menu_nav_op1:
+                        Toast.makeText(getApplicationContext(), "Opcionpulsada", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_nav_op2:
+                        break;
+                    case R.id.menu_nav_op3:
+                        break;
+                    case R.id.menu_nav_fragment1:
+                        fragmentTransaction.replace(R.id.sitioFragment, new FragmentUno());
+                        fragmentTransaction.commit();
+                        break;
+                    case R.id.menu_nav_fragment2:
+                        fragmentTransaction.replace(R.id.sitioFragment, new FragmentDos());
+                        fragmentTransaction.commit();
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+
+        });
     }
 }
